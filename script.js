3ddfc63aaad2e650b2d5a88089c48c5e490f494a
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-27 22:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function call(){
+return new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("Hello, World!");
+	},1000)
+})
+}
+
+call().then((data)=>{
+	const l=document.getElementById("output");
+	l.innerText=data;
+})
